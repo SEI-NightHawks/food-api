87eae804c13fb06dc2a34795ea732f287751118e
Empairim <titusfaulknertull@yahoo.com>
2023-12-08 10:45:38 -0500

    fixed issue with displaying profile name and propfile images on Post compoents

--- food/models.py
+++ food/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic_url = models.CharField(max_length=255, blank=True, null=True)
+    profile_pic_url = models.URLField(max_length=255, blank=True, null=True)
     def __str__(self):
         return self.user.username
 
